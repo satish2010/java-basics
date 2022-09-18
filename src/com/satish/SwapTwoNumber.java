abb497e4253b7b767af171fe9de1db44d7e8aea8
satish2010 <sharmasatish2010@gmail.com>
2022-09-18 10:34:15 +0100

    updating swap two numbers

--- src/com/satish/SwapTwoNumber.java
+++ src/com/satish/SwapTwoNumber.java
@@ -4,6 +4,11 @@ public class SwapTwoNumber {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
+        swapNumber(a, b);
+        System.out.printf("a is %d, b is %d", a, b);
+    }
+
+    public static void swapNumber(int a, int b) {
         b = a + b;
         System.out.println("b " + b);
         a = b - a;
