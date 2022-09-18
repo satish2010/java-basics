2af671495ace4bbd0d77c9aea387e6dc940e692f
satish2010 <sharmasatish2010@gmail.com>
2022-09-18 10:37:42 +0100

    testing

--- src/com/satish/SwapTwoNumber.java
+++ src/com/satish/SwapTwoNumber.java
@@ -4,6 +4,7 @@ public class SwapTwoNumber {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
+        //test
         swapNumber(a, b);
         System.out.printf("a is %d, b is %d", a, b);
     }
